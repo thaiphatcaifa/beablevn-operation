8e023ef3af8c5dd62b8a581caebebe67fea2a788
thaiphatcaifa <thaiphat.caifa@gmail.com>
2026-05-02 01:46:27 +0700

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database"; 
-import { getAuth } from "firebase/auth"; 
-import { getMessaging, isSupported } from "firebase/messaging"; // IMPORT MESSAGING
+import { getAuth } from "firebase/auth"; // 1. Import thêm getAuth
 
 const firebaseConfig = {
   apiKey: "AIzaSyCcDf-QwrU2zkkQan49gSdq6AkjY5JI2rQ",
@@ -16,12 +15,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app); 
-export const auth = getAuth(app); 
-
-// Khởi tạo Messaging an toàn (tránh lỗi trên các trình duyệt không hỗ trợ như Safari cũ)
-export let messaging = null;
-isSupported().then((supported) => {
-  if (supported) {
-    messaging = getMessaging(app);
-  }
-});
+export const auth = getAuth(app); // 2. Export biến auth
